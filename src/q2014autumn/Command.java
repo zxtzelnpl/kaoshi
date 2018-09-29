b6f9ea8bf5f2d33b4b1cad12b4da6d5769e1bb8b
zxt_z <zxt_zel_npl@163.com>
2018-09-29 09:43:16 +0800

    2014年下半年命令模式全对

---
+++ src/q2014autumn/Command.java
@@ -0,0 +1,5 @@
+package q2014autumn;
+
+public interface Command {  //(1)
+    public void execute();
+}
---
+++ src/q2014autumn/LightOffCommand.java
@@ -0,0 +1,14 @@
+package q2014autumn;
+
+public class LightOffCommand implements Command {
+    Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        this.light.off();        //(3)
+    }
+}
